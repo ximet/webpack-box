29704f238224d1a4f596f8d24687d65277d4cddc
ximet <dq.ximet@gmail.com>
2017-08-08 18:20:28 +0300

    add jsdoc

--- src/babelLoader.js
+++ src/babelLoader.js
@@ -1,5 +1,11 @@
-const getBabelLoader = (typeFileArray, presetsList) => {
-    const regularTest = `/\.(${typeFileArray
+/**
+ * Function which create babel-loader object
+ * @param  {array} typeFileList - array type files
+ * @param  {array} presetsList - array type presets
+ * @returns {object}
+ */
+const getBabelLoader = (typeFileList, presetsList) => {
+    const regularTest = `/\.(${typeFileList
                             .map(item => `${item}|`)
                             .join()})$/`
     return {
