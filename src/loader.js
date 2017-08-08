5ceb689c55490fdb995d4606557f80cf6ef4e4dc
ximet <dq.ximet@gmail.com>
2017-08-08 18:25:03 +0300

    add getLoader function

---
+++ src/loader.js
@@ -0,0 +1,23 @@
+/**
+ * Function which create loader object
+ * @param  {string} typeLoader - name loader
+ * @param  {array} typeFileList - array type files
+ * @param  {object} options - object options this loader
+ * @returns {object}
+ */
+const getLoader = (typeLoader, typeFileList, options) => {
+    const regularTest = `/\.(${typeFileList
+                            .map(item => `${item}|`)
+                            .join()})$/`
+    return {
+      test: regularTest,
+      use: {
+        loader: typeLoader,
+        options: options
+      }
+    };
+}
+
+module.exports = {
+    getLoader
+}
