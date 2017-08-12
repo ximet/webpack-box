1010269948511567f79f563b39948986615ac9cf
ximet <dq.ximet@gmail.com>
2017-08-12 19:43:41 +0300

    update babel-loader

--- src/loaders/babelLoader.js
+++ src/loaders/babelLoader.js
@@ -1,6 +1,16 @@
 const { getLoader } = require('./loader.js');
 
+/**
+ * Function which create babel-loader object
+ * @param  {array} typeFileList - array type files
+ * @param  {array} presetsList - array type presets
+ * @returns {object}
+ */
 const getBabelLoader = (typeFileList, presetsList) => {
+    const options = {
+        presets: presetsList
+    };
+
     return getLoader('babel-loader', typeFileList, options)
 }
 
