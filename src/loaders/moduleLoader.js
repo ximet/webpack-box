faeaaf03808498a4479307f7508fa176535d0d3c
ximet <dq.ximet@gmail.com>
2017-10-16 20:20:02 +0300

    update module loader

--- src/loaders/moduleLoader.js
+++ src/loaders/moduleLoader.js
@@ -1,6 +1,11 @@
+const { getBabelLoader } = require('./babelLoader.js');
+const { imageURLLoader, imageFileLoader } = require('./imageLoader.js');
+const { styleLoader, cssLoader } = require('./styleLoader.js');
 
 const getLoaders = (listTypeLoaders) => {
-    
+    listTypeLoaders.map((item) => {
+        
+    })
 }
 
 module.exports = {
